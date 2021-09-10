dcc98788c0f2220eeb5455b12142e7c6668395b8
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-09-10 16:14:56 +0900

    モジュールレベルでグローバルスコープが分けられるので__init__にglobals()を渡すようにした

--- nokogiri/cell.py
+++ nokogiri/cell.py
@@ -11,18 +11,21 @@ if IS_JUPYTER:
     import _ast
     import textwrap
     class cell(ast.NodeVisitor):
+        def __init__(self, g=globals(), *args, **kwargs):
+            self.g = g
+            super().__init__(*args, **kwargs)
         def __call__(self, func):
             args = func.__code__.co_varnames[:func.__code__.co_argcount]
-            self.retval = func(**{arg: globals()[arg] for arg in args})
+            self.retval = func(**{arg: self.g[arg] for arg in args})
             root = ast.parse(textwrap.dedent(inspect.getsource(func)))
             self.visit(root)
             return func
         def visit_Return(self, node):
             if type(node.value) == _ast.Name:
-                globals()[node.value.id] = self.retval
+                self.g[node.value.id] = self.retval
             elif type(node.value) == _ast.Tuple:
                 for elt, val in zip(node.value.elts, self.retval):
-                    globals()[elt.id] = val
+                    self.g[elt.id] = val
             self.generic_visit(node)
     if __name__ == '__main__':
         A = 1
