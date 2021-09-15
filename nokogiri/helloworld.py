32cbafb0bb8a024e9d50c3c3ceb85f1ec42b2a23
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-09-15 17:16:11 +0900

    shared_mapを追加

--- nokogiri/helloworld.py
+++ nokogiri/helloworld.py
@@ -1,2 +1,5 @@
 def helloworld():
     print("helloworld")
+
+if __name__ == "__main__":
+    helloworld()
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='nokogiri',
-    version='0.1.9',
+    version='0.1.10',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=[
