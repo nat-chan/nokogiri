e171bfe908e10b4b5ebf42a96a85b2c2b9ae237f
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2021-09-15 16:18:49 +0900

    tqdm_loadを追加

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='nokogiri',
-    version='0.1.8',
+    version='0.1.9',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=[
