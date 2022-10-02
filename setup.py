df997bcae0799d26fe9ae7c77b60eb241c06834b
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2022-10-02 21:46:57 +0900

    anyparseを追加して0.1.16をリリースまだREADME.mdに使い方載せてない

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ except ImportError:
 
 setup(
     name='nokogiri',
-    version='0.1.15',
+    version='0.1.16',
     url='https://github.com/nat-chan/nokogiri',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
